461436de4a5a46df1963c31195ec7531583f2517
jteoh <jiateoh@gmail.com>
2018-10-31 23:13:14 -0700

    Update multiple iterations script to use sbt and include hardcoded logfile (for reverse compatibility with existing scripts)

--- scripts/multiple_iterations.sh
+++ scripts/multiple_iterations.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-CMD="java -cp /Users/jteoh/Code/perfdebug-baselines/target/scala-2.11/perfdebug-baselines-assembly-0.1.jar "
+# CMD="java -cp /Users/jteoh/Code/perfdebug-baselines/target/scala-2.11/perfdebug-baselines-assembly-0.1.jar "
 CLASS=${1}
 OUTFILE=${2}
+LOGFILE="/tmp/perfdebug-baseline-lastrun.log"
 NUM_ITERATIONS=${3}
 if [ -z "$CLASS" ]; then
   echo "Please specify class name"
@@ -15,14 +16,17 @@ elif [ -z "$NUM_ITERATIONS" ]; then
   exit 3
 fi
 
-CMD+=$CLASS
+# CMD+=$CLASS
+CMD="sbt \"runMain $CLASS\""
 
 rm -f $OUTFILE
-echo "Printing output to $OUTFILE"
+rm -f $LOGFILE
+echo "Printing output to $OUTFILE, log in $LOGFILE"
 echo $CMD
 for ((i = 1; i <= $NUM_ITERATIONS; i++)) ; do 
   echo "SCRIPT: iteration $i"
-  $CMD 2>&1 | fgrep "Collect time: " | sed -En "s/[^0-9]*([0-9]{1,})[^0-9]*/\1/gp" >> $OUTFILE
+  eval $CMD 2>&1 | tee -a $LOGFILE | fgrep "Collect time: " | sed -En "s/[^0-9]*([0-9]{1,})[^0-9]*/\1/gp" >> $OUTFILE
+  # $CMD 2>&1 | fgrep "Collect time: " | sed -En "s/[^0-9]*([0-9]{1,})[^0-9]*/\1/gp" >> $OUTFILE
 done
 
 paste -sd " " $OUTFILE
